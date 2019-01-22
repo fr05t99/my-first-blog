b4b72cafa930c920bb8ca244bb660ab7405c5bbf
frost <dwijrastogi100@gmail.com>
2019-01-22 11:34:33 +0530

    Changed ordering of posts

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts' : posts})
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
